3fe09547c0e6b5fb1f93fde383f59f69708cbf06
T.J. Mosser <tjmosser@gmail.com>
2018-06-07 21:23:02 -0500

    test commit
    this was a small change to test repository

--- Source/Brawl_Ball/PlayerCharacter.cpp
+++ Source/Brawl_Ball/PlayerCharacter.cpp
@@ -8,7 +8,7 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//ability = CreateDefaultSubobject<UDashComponent>(TEXT("MovementAbility"));
+	ability = CreateDefaultSubobject<UDashComponent>(TEXT("MovementAbility"));
 
 	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 
